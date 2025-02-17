8b2b87147cb9e9023b1e6522575d2e3802e98db8
Lakshay25-LA <lakshay1366.becse24@chitkara.edu.in>
2025-02-17 17:40:15 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -1,24 +1,24 @@
-// Your code here...
 #include <stdio.h>
+
 int main() {
-int a,B,C,D,E,F;
-scanf("%d",&a);
-if (a >= 90) {
-    printf("%s",A);
-}
-if else (a >= 80 && a < 90) {
-    printf("%s",B)
-}
-if else (a >= 70 && a < 80){
-    printf("%s",C);
-}
-if else(a >= 60 && a < 70){
-    printf("%s",D);
-}
-else {
-    printf("%s", F);
-}
-return 0; 
-}
+    int a;
+    scanf("%d", &a);
 
+    if (a >= 90) {
+        printf("A");
+    } 
+    else if (a >= 80) {
+        printf("B");
+    } 
+    else if (a >= 70) {
+        printf("C");
+    } 
+    else if (a >= 60) {
+        printf("D");
+    } 
+    else {
+        printf("F");
+    }
 
+    return 0;
+}
