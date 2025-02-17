81c50669014b138acd7cb588d53374b175bcccb8
Lakshay25-LA <lakshay1366.becse24@chitkara.edu.in>
2025-02-17 19:55:01 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
-int main() {
-    int n1, n2;
-    
-    // Taking input for two numbers
-    scanf("%d %d", &n1, &n2);
-    
-    // Checking if n1 is a multiple of n2
-    if (n2 != 0 && n1 % n2 == 0) { 
+int main () {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (b != 0 && a % b == 0) {
         printf("Yes");
-    } else {
+    }
+
+    else {
         printf("No");
     }
-    
     return 0;
 }
