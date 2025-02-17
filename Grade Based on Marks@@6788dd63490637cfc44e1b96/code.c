f83b49349b1d4559b453b295a3b5eae981ac63e9
Lakshay25-LA <lakshay1366.becse24@chitkara.edu.in>
2025-02-17 17:25:45 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -6,13 +6,13 @@ scanf("%d",&a);
 if (a >= 90) {
     printf("A");
 }
-else if (a >= 80 && a < 90) {
+if else (a >= 80 && a < 90) {
     printf("B")
 }
-else if (a >= 70 && a < 80){
+if else (a >= 70 && a < 80){
     printf("C");
 }
-else if (a >= 60 && a < 70){
+if else(a >= 60 && a < 70){
     printf("D");
 }
 else {
